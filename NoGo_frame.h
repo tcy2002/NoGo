b77aa9a643f9be7ff57bf9db51f22066757dfb26
tcy2002 <74089647+tcy2002@users.noreply.github.com>
2021-10-17 00:05:09 +0800

    Update NoGo_frame.h

--- NoGo_frame.h
+++ NoGo_frame.h
@@ -1,6 +1,7 @@
 #ifndef NOGO_FRAME_H_INCLUDED
 #define NOGO_FRAME_H_INCLUDED
 #include <iostream>
+//游戏基础控制
 
 using namespace std;
 
@@ -14,29 +15,33 @@ public:
     static int chosen;
     static int begin;
     static int stop;
+    static int *lastChosen;
     static int state[9][9];
     static bool nothere[9][9];
     static bool checked[9][9];
 
+    //General_frame.cpp
     void xy(int a, int b);
     void input();
+    bool error();
     void initial();
     void gameframe();
     void button();
-    bool display();
-
-    void level();
-    void borw();
+    //Options_frame.cpp
     void restart();
     void open();
     void save();
-
+    void borw();
+    void level();
+    //Game_frame.cpp
     bool win();
-    void choose();
+    int *choose(int lastChosen[2]);
     bool print();
     bool isNoqi(int a, int b);
     void returnok();
     bool renew();
+    void exit();
+    bool HumanAction();
 };
 
 #endif
