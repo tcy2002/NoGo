ef9400784036b2687799b394bc613011b1576dc7
tcy2002 <74089647+tcy2002@users.noreply.github.com>
2021-10-17 00:00:38 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,62 +6,21 @@ int main()
 {
     frame g;
     nogo n;
-    system("mode con cols=47 lines=26");
     system("color f1");
+    system("mode con cols=47 lines=20");
 
     g.initial();
-    g.gameframe();
-    g.borw();
-    g.level();
-    g.gameframe();
-    g.button();
+    g.gameframe(); //初始化框架
+    g.borw();      //选择先后手
+    g.level();     //选择难度级别
+    g.gameframe(); //刷新框架
+    g.button();    //选项
 
     while (1)
     {
-        if (g.win() && (g.stop = 1))
-        {
-            g.xy(1, 0);
-            if (g.player)
-                cout << "黑方获胜";
-            else
-                cout << "白方获胜";
-        }
-        else if (g.player != g.chosen && !g.stop)
-        {
-            if (g.gamelevel == 1)
-                n.level1();
-            else if (g.gamelevel == 2)
-                n.level2();
-            else if (g.gamelevel == 3)
-                n.level3();
-            else
-                n.level4();
-        }
-
-        if (g.win() && (g.stop = 1))
-        {
-            g.xy(1, 0);
-            if (g.player)
-                cout << "黑方获胜";
-            else
-                cout << "白方获胜";
-        }
-
-        do {g.choose();} while (g.x <= 17 && g.nothere[(g.x - 1) / 2][(g.y - 1) / 2] && !g.stop && g.display());
-
-        if (g.x <= 17)
-            !g.stop && g.print() && (g.begin = 0);
-        else if (g.y < 6)
-            g.restart();
-        else if (g.y < 10)
-            g.open();
-        else if (g.y < 14)
-            g.save();
-        else
-        {
-            g.xy(0, 20);
-            system("pause");
-            return 0;
-        }
+        g.exit();                        //玩家落子后判断是否终止
+        n.AIaction();                    //AI落子
+        g.exit();                        //AI落子后判断是否终止
+        if (!g.HumanAction()) return(0);//玩家选点落子
     }
 }
